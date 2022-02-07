80485b8147276bcf3c6caf24964b846b13cd73f0
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-07 18:00:23 -0500

    useEffect hook - examples

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,17 +2,27 @@ import React from "react";
 
 const Search = () => {
   const [term, setTerm] = React.useState('');
+  const [abc, setAbc] = React.useState('');
 
   //the second argument controls when is called the useEffect hook
   /* 
-  no argument: first time only
-  []: when the component is rendered (including first time)
-  [var]: when the component is rendered&the var has changed 
+  []: first time only
+  no argument: when the component is rendered (including first time)
+  [var]:  when the component is rendered&the var has changed 
           (both needs to happened, in case is rerendered but the var has no changed it´s not executed)
+          multiple vars ([abc, cde]) if either
    */
   React.useEffect(() => {
-    
+    console.log('first time')
+  }, []);
+
+  React.useEffect(() => {
+    console.log('every time')
   });
+
+  React.useEffect(() => {
+    console.log('rendered and abc changed')
+  }, [abc]);
 
   return (
     <div>
