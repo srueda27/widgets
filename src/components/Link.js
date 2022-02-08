77bd13f94e2df2dcfedc92c3ada850fdceabd2eb
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-08 09:59:40 -0500

    Add EventListener, change the path, rerendered Route when the path change

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,11 @@ import React from "react";
 const Link = ({className, href, children}) => {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({},'', href);
+
+    //tell the Route components that something has change
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   }
 
   return (
