a9bb2c026675716176facad00dd5709e0633bbbe
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-08 09:34:49 -0500

    Alter Accordion functionality to close on own click

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
+  const [activeClass, setActiveClass] = useState(false);
   
   //Every time a setHook is called the entire component is rerendered
   //excluding the definition
   const onTitleClick = (index) => {
-    setActiveIndex(index)
+    setActiveIndex(index);
+    setActiveClass(!activeClass);
   };
-
+  
   const renderedItems = items.map((item, index) => {
-    const active = index === activeIndex ? 'active' : '';
+    const active = index === activeIndex && activeClass ? 'active' : '';
 
     return (
       <React.Fragment key={item.title}>
