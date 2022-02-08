70c000a9b107a3c49efbe9e88c666e8c6e4b8e39
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-07 22:44:45 -0500

    Add google api translate request. Use useEffect to limit the request through a debounced state

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,15 +1,47 @@
+import axios from "axios";
 import React from "react";
 
 const KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
 
-const Convert = ({language, text}) => {
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = React.useState('');
+  const [debouncedText, setDebouncedText] = React.useState(text);
+
   React.useEffect(() => {
-    
-  }, [language, text])
+    const timerId = setTimeout(() => {
+      setDebouncedText(text)
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  },[text]);
+
+  React.useEffect(() => {
+    if (text.trim() == '') setTranslated('')
+  })
+
+  React.useEffect(() => {
+    if (debouncedText.trim() == '') return
+
+    const doTranslate = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: debouncedText,
+          target: language.value,
+          key: KEY
+        }
+      });
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslate();
+  }, [language, debouncedText])
 
   return (
     <div>
-
+      <h1 className="ui header">{translated}</h1>
     </div>
   );
 }
