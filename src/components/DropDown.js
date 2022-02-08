0ce1915062c80202f2e211148e607b2d4eed1b73
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-07 21:19:22 -0500

    Add eventlistener to document.body

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener('click', () => {
+      setOpen(false);
+    }, { capture: true });
+  }, []);
+
   const renderedOptions = options.map(option => {
+    if (option.value === selected.value) {
+      return null
+    }
+
     return (
-      <div 
-      key={option.value} 
-      className="item" 
-      onClick={() => onSelectedChange(option)}>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}>
         {option.label}
       </div>
     );
@@ -16,10 +28,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
